5f2ab7b44f9939190e49e1a70fad117c92037957
Stanislav Pugachev <129574544+stanislav-pugachev@users.noreply.github.com>
2023-04-01 05:51:35 +0100

    Первоначальные файлы для проекта MakeCode

---
+++ main.ts
@@ -0,0 +1,37 @@
+input.onButtonPressed(Button.A, function () {
+    шар.turn(Direction.Left, 90)
+    шар.move(1)
+    basic.pause(200)
+    шар.move(1)
+    basic.pause(200)
+    шар.move(-1)
+    basic.pause(200)
+    шар.move(-1)
+    шар.turn(Direction.Right, 90)
+})
+input.onButtonPressed(Button.AB, function () {
+    шар.turn(Direction.Left, 90)
+    шар.move(1)
+    basic.pause(700)
+    шар.move(-1)
+    шар.turn(Direction.Right, 90)
+})
+let sprite: game.LedSprite = null
+let шар: game.LedSprite = null
+шар = game.createSprite(1, 4)
+basic.forever(function () {
+    sprite = game.createSprite(4, 4)
+    basic.pause(100)
+    sprite.turn(Direction.Right, 180)
+    for (let index = 0; index < 4; index++) {
+        sprite.move(1)
+        basic.pause(500)
+        if (sprite.isTouching(шар)) {
+            game.gameOver()
+        }
+    }
+    if (sprite.isTouchingEdge()) {
+        sprite.delete()
+        game.addScore(1)
+    }
+})
